138b369084206d5bfe4ba096506b6387ca5f33f1
masfa-r <masfa.rafi99@gmail.com>
2020-11-24 21:06:49 +0500

    first commit

---
+++ UBIT.IPT.SERVER/Model/MarksInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UBIT.IPT.SERVER.Model
+{
+    public class MarksInfo
+    {
+        public string subjectName { get; set; }
+        public int subjectMarksObtained { get; set; }
+    }
+}
